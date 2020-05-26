8c4e96b12c3414d0e8c186d58e5bdb8f0fa2fee2
Atsuyagoto <go11ucqrdef4h26@outlook.jp>
2020-05-26 16:35:39 +0900

    initial commit

--- DailyReportSystem/Models/IdentityModels.cs
+++ DailyReportSystem/Models/IdentityModels.cs
@@ -4,11 +4,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
+using System.ComponentModel; // 追記
+using System.ComponentModel.DataAnnotations; // 追記
+using System; // 追記
+
 namespace DailyReportSystem.Models
 {
-    // ApplicationUser クラスにさらにプロパティを追加すると、ユーザーのプロファイル データを追加できます。詳細については、https://go.microsoft.com/fwlink/?LinkID=317594 を参照してください。
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser//プロパティを4つApplicationUserクラスに追加
     {
+        [DisplayName("従業員名")]
+        [Required(ErrorMessage = "氏名を入力してください。")]
+        [StringLength(20, ErrorMessage = "{0}は{1}文字を超えることはできません。")]
+        public string EmployeeName { get; set; } // 名前
+
+        [DisplayName("作成日付")]
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedAt { get; set; } // データの作成された日時
+
+        [DisplayName("更新日付")]
+        [DataType(DataType.DateTime)]
+        public DateTime? UpdatedAt { get; set; } // データの更新された日時
+
+        public int DeleteFlg { get; set; } // Userの削除フラグ
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // authenticationType が CookieAuthenticationOptions.AuthenticationType で定義されているものと一致している必要があります
@@ -17,6 +35,8 @@ namespace DailyReportSystem.Models
             return userIdentity;
         }
     }
+    // ApplicationUser クラスにさらにプロパティを追加すると、ユーザーのプロファイル データを追加できます。詳細については、https://go.microsoft.com/fwlink/?LinkID=317594 を参照してください。
+   
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
