5eca5c2869ba5b175c3f96dc26796c3b017b5c37
atsuya_goto <at-goto@i-enter.co.jp>
2020-05-27 10:39:06 +0900

    modify employees index

---
+++ DailyReportSystem/Models/EmployeeViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+
+namespace DailyReportSystem.Models
+{
+    // ファイル名とクラス名が違うので注意しましょう。
+    public class EmployeesIndexViewModel
+    {
+        public string Id { get; set; } // IdentityのId
+
+        [DisplayName("メールアドレス")]
+        public string Email { get; set; }
+
+        [DisplayName("氏名")]
+        public string EmployeeName { get; set; }
+
+        public int DeleteFlg { get; set; } // Userの削除フラグ
+    }
+
+}
